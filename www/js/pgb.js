0d8fadfcc8b6b987e2f35ff776c43b194fac18b4
Radekl2 <radek.smyko@wp.pl>
2018-03-11 17:42:24 +0100

    bug fixes

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -1,8 +1,10 @@
 function init() {
 	document.addEventListener("deviceready",onDeviceReady, false);
+	
 }
 
 function onDeviceReady() {
+	navigator.splashscreen.show();
 	navigator.notification.beep(1);
 }
 
